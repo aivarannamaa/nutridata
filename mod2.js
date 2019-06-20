ede275ae4d2ab7d0640b78255b04731fe4def7e5
Aivar Annamaa <aivarannamaa@users.noreply.github.com>
2019-06-20 07:37:55 +0300

    Jäta tühjad söögikorrad tabelis vahele

--- mod2.js
+++ mod2.js
@@ -88,12 +88,9 @@ function createSummaryTable(menu) {
             prot += (recipe["amount"] / 100) * recipe["nutridata"]["protein"];
         });
         
-	/*
-        if (meal["nameEst"] == "Lõunaoode" 
-            &&  kcal == 0 && carb == 0 && fat == 0 && prot == 0) {
+        if (kcal == 0 && carb == 0 && fat == 0 && prot == 0) {
             return;
         }
-	*/
         
         
         var ratio = fat / (carb + prot);
